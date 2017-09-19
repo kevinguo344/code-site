0af7102625d80034bfb0beda610f3da77ae5d537
Kevin Guo <kg344@cornell.edu>
2017-09-18 22:24:33 -0400

    Some bug fixes

--- server.js
+++ server.js
@@ -2,7 +2,20 @@
 
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 8080;
+	port = process.env.PORT || 8080,
+	mongoose = require('mongoose'),
+	Location = require('./api/models/locationModel'),
+	Content = require('./api/models/contentModel'),
+	bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/db');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/routes');
+routes(app);
 
 app.listen(port);
 console.log("Started api at port: " + port);
